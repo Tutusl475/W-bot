cf44407a6da48c714e20d15b7b11a93c50783a48
Tutusl475 <nadunnawodya69@gmail.com>
2024-09-13 19:24:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: LupzCIgA#yn5-JlvuVdpC3pXGjqCESId6o1zl8tCtNV4dzfTCJ0s",
+SESSION_ID: "0msmTJrD#teQpWm4dYfiWwwRve1eLSYahclmtK79OgFGgBJ4JAcA",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
